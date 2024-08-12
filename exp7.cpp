dfba49a21eda74d967300d0f48c355c798371fb4
Sudeeksha2802 <sudeeksha.23bce11136@vitbhopal.ac.in>
2024-08-13 00:46:43 +0530

    Update exp7.cpp

--- exp7.cpp
+++ exp7.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+struct Book {
+    int book_id;
+    string author_name;
+    double price;
+    int no_of_pages;
+    string publisher;
+    int year_of_publishing;
+};
+
+// Function to input book details
+void inputBookDetails(Book& book) {
+    cout << "Enter book ID: ";
+    cin >> book.book_id;
+    cin.ignore(); // To ignore the new line after the integer input
+
+    cout << "Enter author name: ";
+    getline(cin, book.author_name);
+
+    cout << "Enter price: ";
+    cin >> book.price;
+
+    cout << "Enter number of pages: ";
+    cin >> book.no_of_pages;
+    cin.ignore(); 
