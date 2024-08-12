9e5a0ea4b6a56f84e63daf76cd59326815aeab49
ashwinrajan-159 <143933017+ashwinrajan-159@users.noreply.github.com>
2024-08-13 00:32:19 +0530

    Update exp6.cpp

---
+++ exp6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Date {
+private:
+    int day, month, year;
+
+    bool isLeapYear(int year) const {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    // to get the number of days in a given month
+    int daysInMonth(int month, int year) const {
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 4: case 6: case 9: case 11:
+                return 30;
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            default:
+                return 0;
+        }
+    }
+
+public:
