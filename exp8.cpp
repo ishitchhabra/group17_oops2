9b64a8716e7f67e0fc799ebea5ff94019dba041e
ashwinrajan-159 <143933017+ashwinrajan-159@users.noreply.github.com>
2024-08-13 00:56:40 +0530

    Update exp8.cpp

--- exp8.cpp
+++ exp8.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+template <typename T>
+class Vector {
+private:
+    T elements[100];
+    int size;
+
+public:
+    Vector() : size(0) {}
+
+    void addElement(T element) {
+        if (size < 100) {
+            elements[size++] = element;
+        }
+    }
+
+    T smallestElement() {
+        if (size == 0) {
+            cout << "Vector is empty." << endl;
+            return T();
+        }
+
+        T smallest = elements[0];
+        for (int i = 1; i < size; i++) {
+            if (elements[i] < smallest) {
+                smallest = elements[i];
+            }
+        }
+        return smallest;
+    }
